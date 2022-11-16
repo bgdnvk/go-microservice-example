ad6087fb8da76f531403a64bda7d0242088a1b76
bgdnvk <bnovykov@gmail.com>
2022-11-16 04:19:17 +0100

    comment fix

--- migrations/2_comments.up.sql
+++ migrations/2_comments.up.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS comments (
     id SERIAL PRIMARY KEY,
     comment VARCHAR NOT NULL,
-    comment_date DATE DEFAULT CURRENT_DATE,
     user_id BIGINT REFERENCES users(id)
 );
 
--- pkg/db/models/comment.go
+++ pkg/db/models/comment.go
@@ -3,6 +3,5 @@ package models
 type Comment struct {
 	ID      int64  `json:"id"`
 	Comment string `json:"comment"`
-	//time
-	User *User `pg:"rel-hasone" json:"user"`
+	User *User `pg:"rel:has-one" json:"user"`
 }
