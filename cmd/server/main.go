6f697f4cbd5360d66f9ab060ccf6512c9f2d21cd
bgdnvk <bnovykov@gmail.com>
2022-11-19 11:32:34 +0100

    comments

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -11,14 +11,16 @@ import (
 
 func main() {
 	log.Print("server has started")
-
+	//start the db
 	pgdb, err := db.StartDB()
 	if err != nil {
 		log.Printf("error starting the database %v", err)
 	}
-
+	//get the router of the API by passing the db
 	router := api.StartAPI(pgdb)
+	//get the port from the environment variable
 	port := os.Getenv("PORT")
+	//pass the router and start listening with the server
 	err = http.ListenAndServe(fmt.Sprintf(":%s", port), router)
 	if err != nil {
 		log.Printf("error from router %v\n", err)
